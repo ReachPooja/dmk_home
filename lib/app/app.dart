c1efe695c08e5dda0a9db74d35965baa1eab0f26
ReachPooja <reachpoojaprajapat@gmail.com>
2022-09-23 20:37:33 +0530

    feat: portrait only

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -3,6 +3,7 @@ import 'package:dmk_home/src/home/bloc/meal_bloc.dart';
 import 'package:dmk_home/src/home/repository/meal_respository.dart';
 import 'package:dmk_home/src/home/view/home_view.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 
@@ -12,6 +13,11 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
+
     return BlocProvider(
       lazy: false,
       create: (context) => MealBloc(MealRepository())
@@ -19,7 +25,7 @@ class MyApp extends StatelessWidget {
         ..add(HasPopularRecipesData())
         ..add(HasSuggestedMealData()),
       child: MaterialApp(
-        title: 'Flutter Demo',
+        debugShowCheckedModeBanner: false,
         theme: ThemeData(
           fontFamily: 'Montserrat',
           primarySwatch: Colors.lightGreen,
